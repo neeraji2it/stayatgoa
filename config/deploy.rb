ab9e426ea0a726776e08d1ba09c9a5200b3ba44b
neeraj <t.garg28@gmail.com>
2016-04-07 13:09:56 +0530

    changed deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+#set :application, "set your application name here"
+#set :repository,  "set your repository location here"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -23,3 +23,63 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+set :stages, %w(production)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+require 'bundler/capistrano'
+
+role (:web) {"#{domain}"}
+role (:app) {"#{domain}"}
+role (:db) { ["#{domain}", {:primary => true}] }
+
+# Set the deploy branch to the current branch
+set :application, "livegoa"
+set :scm, :git
+set (:repository) { "#{gitrepo}" }
+set (:deploy_to) { "#{deploydir}" }
+set :scm_user, "root"
+set :keep_releases, 2
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+desc "Symlinks database.yml, mailer.yml file from shared directory into the latest release"
+task :symlink_shared, :roles => [:app, :db] do
+  run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  run "ln -s #{shared_path}/system #{latest_release}/system"
+end
+
+after "deploy:update", "deploy:cleanup"
+# after "deploy:stop",    "delayed_job:stop"
+# after "deploy:start",   "delayed_job:start"
+# after "deploy:restart", "delayed_job:restart"
+# 
+
+#task :restart_delayed_job, :roles => [:app, :db] do
+#  run "RAILS_ENV=serverdev script/delayed_job stop"
+#  run "RAILS_ENV=serverdev script/delayed_job start"
+#end
+#
+#after 'deploy:finalize_update', :restart_delayed_job
+
+after 'deploy:finalize_update', :symlink_shared
+#after 'deploy:finalize_update', 'deploy:extractions'
+
+namespace :deploy do
+  desc "Reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+   
---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :domain, "104.251.216.145"
+set :rails_env, 'production'
+set :gitrepo, "git@github.com:neeraji2it/stayatgoa.git"
+set :deploydir, "/var/www/livegoa.com/public_html"
+set :user, "shivraj"
+set :use_sudo, false
+                  
